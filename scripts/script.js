068882403e4c1f1840309797dbbe4d3fe67a348d
Furkan Tura <ftura7@gmail.com>
2023-11-16 18:47:15 +0100

    Nav menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("hi");
+
+function OpenMenu() {
+    var x = document.querySelector(".Content");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
