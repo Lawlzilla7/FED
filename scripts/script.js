37c76e4756ac1adec44be0108cc0009ed1eb2b45
Furkan Tura <ftura7@gmail.com>
2023-11-23 12:56:44 +0100

    Js burger to cross

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
 // JavaScript Document
 console.log("hi");
 
+var Burger = document.getElementById('Burger');
+
 function OpenMenu() {
     var x = document.querySelector(".NavContent");
     if (x.style.display === "grid") {
       x.style.display = "none";
+      Burger.src = 'images/Burger.svg'
     } else {
       x.style.display = "grid";
+      Burger.src = 'images/Cross.svg'
     }
   }
