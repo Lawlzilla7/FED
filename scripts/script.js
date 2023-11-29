46b3f80b10a0a044a0285ebbc53ff916ad280626
Furkan Tura <ftura7@gmail.com>
2023-11-29 12:08:45 +0100

    Media section

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,26 @@
 console.log("hi");
 
 var Burger = document.getElementById('Burger');
+var CarousselButtonsList = document.querySelector('.CarouselButton');
 
 function OpenMenu() {
-    var x = document.querySelector(".NavContent");
-    if (x.style.display === "grid") {
-      x.style.display = "none";
-      Burger.src = 'images/Burger.svg'
-    } else {
-      x.style.display = "grid";
-      Burger.src = 'images/Cross.svg'
-    }
+  var x = document.querySelector(".NavContent");
+  if (x.style.display === "grid") {
+    x.style.display = "none";
+    Burger.src = 'images/Burger.svg'
+  } else {
+    x.style.display = "grid";
+    Burger.src = 'images/Cross.svg'
   }
+}
+
+// function ButtonActive() {
+//   CarousselButtonsList.array.forEach(element => {
+//   });(CarousselButton => {
+//     CarousselButton.addeventlistener('click', () => {
+//       document.querySelector('.CarouselButtonActive').classList.
+//         remove('.CarouselButtonActive');
+//         CarousselButton.classList.add('.CarouselButtonActive');
+//     })
+//   });
+// }
