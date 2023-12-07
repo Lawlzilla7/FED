ee89396004795c8d1663480582009ee04254184a
Furkan Tura <ftura7@gmail.com>
2023-12-07 12:21:30 +0100

    Special video done

--- scripts/script.js
+++ scripts/script.js
@@ -36,4 +36,31 @@ function SwitchMediaContent() {
 // Attach the function to the 'change' event of the dropdown
 document.getElementById('MediaSelect').addEventListener('change', SwitchMediaContent);
 
+VideoFrame = document.getElementById('Special-Video');
 
+function Setvideo(Url) {
+  switch (Url) {
+    case 'Strength':
+      VideoFrame.src = 'https://www.youtube.com/embed/Yuo7AuDGNN8';
+      break;
+    case 'Perception':
+      VideoFrame.src = 'https://www.youtube.com/embed/Vw21X2jKwCM';
+      break;
+    case 'Endurance':
+      VideoFrame.src = 'https://www.youtube.com/embed/paYU1neP3xM';
+      break;
+    case 'Charisma':
+      VideoFrame.src = 'https://www.youtube.com/embed/X95lZ56ddj4';
+      break;
+    case 'Intelligence':
+      VideoFrame.src = 'https://www.youtube.com/embed/H6yt-43Vp1M';
+      break;
+    case 'Agility':
+      VideoFrame.src = 'https://www.youtube.com/embed/bQ_okO2tLN0';
+      break;
+    case 'Luck':
+      VideoFrame.src = 'https://www.youtube.com/embed/Bke9wvob8Ls';
+      break;
+
+  }
+}
